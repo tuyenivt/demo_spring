df7fe5d724ee085a4f93aefa6763bab16c046377
Tuyen Le Chau <tuyenivt@gmail.com>
2018-02-08 21:49:38 +0700

    change to setter dependency injection

--- src/main/java/com/coloza/sample/spring/TennisCoach.java
+++ src/main/java/com/coloza/sample/spring/TennisCoach.java
@@ -11,10 +11,22 @@ public class TennisCoach implements Coach {
 
     private FortuneService fortuneService;
 
+    public TennisCoach() {
+        System.out.println("TennisCoach: inside default contructor");
+    }
+
+    @Autowired
+    public void setFortuneService(FortuneService fortuneService) {
+        System.out.println("TennisCoach: setFortuneService");
+        this.fortuneService = fortuneService;
+    }
+
+    /*
     @Autowired
     public TennisCoach(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
     }
+    */
 
     @Override
     public String getDailyWorkout() {
