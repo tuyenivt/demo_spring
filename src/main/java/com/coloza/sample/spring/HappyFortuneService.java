c11f624a167485644d38ebf71994cd935ff175fa
Zero <tuyenivt@gmail.com>
2018-03-03 15:21:55 +0700

    test inject value from properties file

--- src/main/java/com/coloza/sample/spring/HappyFortuneService.java
+++ src/main/java/com/coloza/sample/spring/HappyFortuneService.java
@@ -1,13 +1,17 @@
 package com.coloza.sample.spring;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HappyFortuneService implements FortuneService {
 
+    @Value("${account.email}")
+    private String email;
+
     @Override
     public String getFortune() {
-        return "Today is your lucky day!";
+        return "Today is your lucky day! (" + email + ")";
     }
 
 }
