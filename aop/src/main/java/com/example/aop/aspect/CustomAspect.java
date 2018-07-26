9cbafb1f176f49043c9ba7900c215eacae8dd818
Tuyen Le Chau <Tuyen.LeChau@nashtechglobal.com>
2018-07-26 09:12:18 +0700

    apply pointcut declaration

--- aop/src/main/java/com/example/aop/aspect/CustomAspect.java
+++ aop/src/main/java/com/example/aop/aspect/CustomAspect.java
@@ -3,14 +3,19 @@ package com.example.aop.aspect;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class CustomAspect {
 
-    // Pointcut and Advice
-    @Around("@annotation(LogExecutionTime)")
+    // Pointcut declaration
+	@Pointcut("@annotation(LogExecutionTime)")
+	private void forAnnotationLogExecutionTime() {}
+
+	// Advice
+    @Around("forAnnotationLogExecutionTime()")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
         System.out.println("joinPoint - around - " + joinPoint.getSignature() + " - begin");
