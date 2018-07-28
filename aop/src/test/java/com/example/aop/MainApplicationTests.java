31bf13626756fe3b6c4c3ed0c60bd0736d99cd3c
Tuyen Le Chau <tuyenivt@gmail.com>
2018-07-28 20:36:30 +0700

    refactor code

--- aop/src/test/java/com/example/aop/MainApplicationTests.java
+++ aop/src/test/java/com/example/aop/MainApplicationTests.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.example.aop.service.Service;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MainApplicationTests {
