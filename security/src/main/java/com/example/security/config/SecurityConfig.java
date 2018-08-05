238e100802a2734fa11a75d3ae3f0a143be1a99e
Tuyen Le Chau <tuyenivt@gmail.com>
2018-08-05 20:25:14 +0700

    demo security with custom login form

--- security/src/main/java/com/example/security/config/SecurityConfig.java
+++ security/src/main/java/com/example/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.security.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -11,13 +12,24 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception { // config user
+    @Override // configure users
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         @SuppressWarnings("deprecation") // for demo only
         UserBuilder users = User.withDefaultPasswordEncoder();
         auth.inMemoryAuthentication()
             .withUser(users.username("john").password("123456").roles("EMPLOYEE"))
             .withUser(users.username("peter").password("123456").roles("MANAGER"))
             .withUser(users.username("frank").password("123456").roles("ADMIN"));
+    }
+
+    @Override // configure security of web paths in application, login, logout, etc
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin()
+                .loginPage("/my-login/")
+                .loginProcessingUrl("/my-authenticate")
+            .permitAll();
     }
 }
