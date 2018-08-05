eacc96924ac9b1ec8652f530f97a463a88d44696
Zero <tuyenivt@gmail.com>
2018-08-05 20:25:14 +0700

    demo security with custom login form

--- security/src/main/java/com/example/security/controller/HomeController.java
+++ security/src/main/java/com/example/security/controller/HomeController.java
@@ -3,14 +3,14 @@ package com.example.security.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @Value("${spring.application.name}")
     String appName;
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("appName", appName);
         return "home/index";
