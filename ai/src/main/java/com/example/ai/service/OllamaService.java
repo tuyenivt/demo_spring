739408fc750f33cdfeef3f129d4119442109ba94
Tuyen Le Chau <tuyenivt@gmail.com>
2025-05-28 21:17:40 +0700

    [ai] implement chat memory

--- ai/src/main/java/com/example/ai/service/OllamaService.java
+++ ai/src/main/java/com/example/ai/service/OllamaService.java
@@ -1,6 +1,7 @@
 package com.example.ai.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.document.Document;
@@ -17,11 +18,11 @@ import java.util.List;
 @Service
 public class OllamaService {
 
-    private final List<String> QDRANT_DOCS = List.of("docs/platform-usage.txt", "docs/insurance-policy.txt");
+    private static final List<String> DOCS = List.of("docs/platform-usage.txt", "docs/insurance-policy.txt");
     private final ChatClient chatClient;
 
     OllamaService(VectorStore vectorStore, ChatClient.Builder builder, ChatMemory chatMemory) {
-        QDRANT_DOCS.stream().map(this::loadDocumentsFromFile).forEach(vectorStore);
+        DOCS.stream().map(this::loadDocumentsFromFile).forEach(vectorStore);
 
         var system = """
                 You are an AI-powered assistant designed to support patients using a telehealth service called HealthConnect.\s
@@ -37,7 +38,11 @@ public class OllamaService {
                 Keep your responses clear, professional, and supportive. If no specific data is provided for a request,\s
                 let the user know and suggest checking back later or contacting support.
                 """;
-        this.chatClient = builder.defaultAdvisors(QuestionAnswerAdvisor.builder(vectorStore).build()).defaultSystem(system).build();
+        this.chatClient = builder
+                .defaultAdvisors(
+                        QuestionAnswerAdvisor.builder(vectorStore).build(),
+                        PromptChatMemoryAdvisor.builder(chatMemory).build())
+                .defaultSystem(system).build();
     }
 
     private List<Document> loadDocumentsFromFile(String filePath) {
@@ -50,6 +55,6 @@ public class OllamaService {
     }
 
     public String getAnswer(String userId, String question) {
-        return this.chatClient.prompt().user(question).call().content();
+        return this.chatClient.prompt().user(question).advisors(a -> a.param(ChatMemory.CONVERSATION_ID, userId)).call().content();
     }
 }
