6ed8e68a10654f253fee93f48b5171dd0f6baebc
Tuyen Le Chau <tuyenivt@gmail.com>
2025-12-26 08:20:58 +0700

    [rate-limiting] add redisConnection config

---
+++ rate-limiting/README.md
@@ -0,0 +1,8 @@
+# rate-limiting
+
+## API Tests
+
+```shell
+curl localhost:8080/api/hello
+curl localhost:8080/api/orders -H "X-USER-ID: 12345"
+```
--- rate-limiting/src/main/java/com/example/ratelimiting/ratelimit/RedisBucketConfig.java
+++ rate-limiting/src/main/java/com/example/ratelimiting/ratelimit/RedisBucketConfig.java
@@ -2,12 +2,28 @@ package com.example.ratelimiting.ratelimit;
 
 import io.github.bucket4j.redis.lettuce.Bucket4jLettuce;
 import io.github.bucket4j.redis.lettuce.cas.LettuceBasedProxyManager;
+import io.lettuce.core.RedisClient;
 import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.codec.ByteArrayCodec;
+import io.lettuce.core.codec.RedisCodec;
+import io.lettuce.core.codec.StringCodec;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
 public class RedisBucketConfig {
+
+    @Bean(destroyMethod = "close")
+    public StatefulRedisConnection<String, byte[]> redisConnection(RedisConnectionFactory redisConnectionFactory) {
+        var lettuceFactory = (LettuceConnectionFactory) redisConnectionFactory;
+        var nativeClient = lettuceFactory.getNativeClient();
+        if (!(nativeClient instanceof RedisClient redisClient)) {
+            throw new IllegalStateException("Lettuce RedisClient is not available.");
+        }
+        return redisClient.connect(RedisCodec.of(new StringCodec(), new ByteArrayCodec()));
+    }
 
     @Bean
     public LettuceBasedProxyManager<String> lettuceProxyManager(StatefulRedisConnection<String, byte[]> redisConnection) {
